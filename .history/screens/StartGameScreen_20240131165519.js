fe09952fabeb61539548609b81a5306f63f549b2
Sy Le <sycung9001@gmail.com>
2024-01-31 16:57:43 +0700

    first commit

---
+++ .history/screens/StartGameScreen_20240131165519.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton />
+    </View>
+  );
+}
+
+export default StartGameScreen;
